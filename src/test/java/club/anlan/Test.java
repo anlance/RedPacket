f93bc8986116a7745cff1820a0d2f4f7342e5d2c
anlan <anlan1996@qq.com>
2019-04-28 20:16:04 +0800

    add  抢红包算法

--- src/test/java/club/anlan/Test.java
+++ src/test/java/club/anlan/Test.java
@@ -16,9 +16,7 @@ import redis.clients.jedis.JedisPoolConfig;
 import java.sql.Timestamp;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
-import java.util.List;
-import java.util.Random;
-import java.util.UUID;
+import java.util.*;
 
 public class Test {
 
@@ -33,9 +31,43 @@ public class Test {
 
     }
 
-    @org.junit.Test
-    public void testTime() {
-//        String sql2 = ","+ Timestamp.valueOf(df.format(new Timestamp(Long (1556369020061)))+",'");
+    // 二倍均值法
+    List<Integer> generatePacketsByDoubleMean(int people, int money) {
+        List<Integer> packets = new ArrayList<>();
+        Random random = new Random();
+        while (people > 1) {
+            int p = random.nextInt(2 * money / people);
+            packets.add(p);
+            money -= p;
+            people--;
+        }
+        packets.add(money);
+        return packets;
+    }
 
+    // 线段切割法
+    List<Integer> generatePacketsByLineCutting(int people, int money) {
+        List<Integer> packets = new ArrayList<>();
+        Random random = new Random();
+        Set<Integer> points = new TreeSet<>();
+        while (points.size() < people - 1) {
+            points.add(random.nextInt(money - 1));
+        }
+        points.add(money);
+        int pre = 0;
+        for (int p : points) {
+            packets.add(p - pre);
+            pre = p;
+        }
+        return packets;
+    }
+
+    @org.junit.Test
+    public void testGrabPackets() {
+        List<Integer> list1 = generatePacketsByDoubleMean(20,100);
+        List<Integer> list2 = generatePacketsByLineCutting(20,100);
+        list1.stream().forEach(item->System.out.print(item+" "));
+        System.out.println("fakes===========");
+        list2.stream().forEach(item->System.out.print(item+" "));
     }
 }
